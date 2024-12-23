38e1242fe251bc4e7a12006746bab6a4595f9f65
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-23 15:53:07 +0600

    Add Appbar done

---
+++ lib/app/widget/app_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ird_skill_task/app/utils/app_color.dart';
+import 'package:ird_skill_task/app/widget/text.dart';
+
+import '../utils/app_assets.dart';
+
+class AppsBar extends StatelessWidget {
+  const AppsBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ModifiedText(
+                text: 'Location',
+                color: AppColor.grey,
+                size: 12,
+                fontWeight: FontWeight.w400,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Row(
+                children: [
+                  ModifiedText(
+                    text: 'Jakarta',
+                    color: Colors.black,
+                    size: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  SvgPicture.asset(
+                    AppAssets.downArrow,
+                    height: 10,
+                    width: 10,
+                  )
+                ],
+              ),
+            ],
+          ),
+          Container(
+            height: 22,
+            width: 17,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppAssets.notifications),
+              ),
+            ),
+            child: Align(
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  AppAssets.ellipse,
+                  height: 8,
+                  width: 8,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
