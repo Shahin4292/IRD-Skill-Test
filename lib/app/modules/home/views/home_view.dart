c7b1a07ed0f7659a45ead10b8769d19272c18adb
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-24 15:51:52 +0600

    almost done

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,10 +6,12 @@ import 'package:get/get.dart';
 import 'package:ird_skill_task/app/utils/app_assets.dart';
 import 'package:ird_skill_task/app/utils/app_color.dart';
 
-import '../../../utils/global_image.dart';
-import '../../../widget/app_bar.dart';
-import '../../../widget/text.dart';
+import '../../../common/app_bar.dart';
+import '../../../common/text.dart';
 import '../controllers/home_controller.dart';
+import '../widget/button_widget.dart';
+import '../widget/list_item.dart';
+import '../widget/product_tile.dart';
 
 class HomeView extends GetView<HomeController> {
   HomeView({super.key});
@@ -19,6 +21,19 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            ListTile(
+              title: Text("text 1"),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("text 2"),
+              onTap: () {},
+            )
+          ],
+        )),
         backgroundColor: Colors.white,
         body: SafeArea(
           child: Padding(
@@ -55,73 +70,32 @@ class HomeView extends GetView<HomeController> {
                     SizedBox(
                       width: 8,
                     ),
-                    Container(
-                      height: 48,
-                      width: 48,
-                      decoration: BoxDecoration(
-                          gradient: GradientColor.gradientColor,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Center(
-                        child: SvgPicture.asset(
-                          AppAssets.menu,
-                          height: 16,
-                          width: 16,
-                        ),
-                      ),
-                    ),
+                    Builder(
+                        builder: (context) => GestureDetector(
+                              onTap: () {
+                                Scaffold.of(context).openDrawer();
+                              },
+                              child: Container(
+                                height: 48,
+                                width: 48,
+                                decoration: BoxDecoration(
+                                    gradient: GradientColor.gradientColor,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Center(
+                                  child: SvgPicture.asset(
+                                    AppAssets.menu,
+                                    height: 16,
+                                    width: 16,
+                                  ),
+                                ),
+                              ),
+                            ))
                   ],
                 ),
                 SizedBox(
                   height: 18,
                 ),
-                Obx(
-                  () => SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: List.generate(
-                        homeController.buttonName.length,
-                        (index) {
-                          return Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 12),
-                            child: GestureDetector(
-                              onTap: () {
-                                homeController.selectedIndex.value = index;
-                              },
-                              child: Container(
-                                height: 34,
-                                width: 68,
-                                decoration: BoxDecoration(
-                                  gradient:
-                                      homeController.selectedIndex.value ==
-                                              index
-                                          ? GradientColor.gradientColor
-                                          : ColorGradient.gradientColor,
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    homeController.buttonName[index],
-                                    style: TextStyle(
-                                      color:
-                                          homeController.selectedIndex.value ==
-                                                  index
-                                              ? Colors.white
-                                              : AppColor.darkGrey,
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ),
+                ButtonWidget(homeController: homeController),
                 SizedBox(
                   height: 27,
                 ),
@@ -149,115 +123,7 @@ class HomeView extends GetView<HomeController> {
                 SizedBox(
                   height: 26,
                 ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: imageList.map((image) {
-                      return GestureDetector(
-                        onTap: () {
-                          // Navigate to the details screen
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) => ImageDetailsScreen(
-                          //       imageUrl: image["url"]!,
-                          //       title: image["title"]!,
-                          //       description: image["description"]!,
-                          //     ),
-                          //   ),
-                          // );
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          // child: ClipRRect(
-                          //   borderRadius: BorderRadius.circular(10),
-                          //   child: Image.network(
-                          //     image["url"]!,
-                          //     width: 222,
-                          //     height: 272,
-                          //     fit: BoxFit.cover,
-                          //   ),
-                          // ),
-                          child: Container(
-                            height: 272,
-                            width: 222,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  image["url"]!,
-                                ),
-                                fit: BoxFit
-                                    .cover, // Adjust image to fill the container
-                              ),
-                            ),
-                            child: Stack(
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.only(top: 20, left: 125),
-                                  child: Container(
-                                    height: 24,
-                                    width: 73,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                      color: AppColor.cyan,
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        SvgPicture.asset(
-                                          AppAssets.locationsIcon,
-                                          height: 12,
-                                          width: 10,
-                                        ),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        ModifiedText(
-                                          text: '1.8 km',
-                                          size: 12,
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.white,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Positioned(
-                                  left: 20,
-                                  bottom: 40,
-                                  child: Column(
-                                    children: [
-                                      ModifiedText(
-                                        text: 'Dreamsville House',
-                                        size: 16,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                      SizedBox(
-                                        width: 8,
-                                      ),
-                                      ModifiedText(
-                                        text: 'Jl. Sultan Iskandar Muda',
-                                        size: 12,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ),
+                ProductTile(),
                 SizedBox(
                   height: 32,
                 ),
@@ -285,40 +151,8 @@ class HomeView extends GetView<HomeController> {
                 SizedBox(
                   height: 24,
                 ),
-                // Container(
-                //   width: MediaQuery.sizeOf(context).width,
-                //   height: MediaQuery.sizeOf(context).height,
-                //   color: Colors.red,
-                //   child: Column(
-                //     mainAxisSize: MainAxisSize.min,
-                //     children: [
-                //       Text("data")
-                //     ],
-                //   ),
-                // )
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: imageItem.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 74,
-                              height: 70,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Image.network(
-                                imageItem[index],
-                              ),
-                            ),
-                            Text("data")
-                          ],
-                        ),
-                      );
-                    },
-                  ),
+                  child: ListItem(),
                 ),
               ],
             ),
