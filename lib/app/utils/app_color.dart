cecfacf6737cf941d501611f4ddb23702522061d
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-24 16:43:18 +0600

    add done

--- lib/app/utils/app_color.dart
+++ lib/app/utils/app_color.dart
@@ -4,6 +4,7 @@ class AppColor {
   static const Color grey = Color(0xFF838383);
   static const Color whiteSmoke = Color(0xFFF7F7F7);
   static const Color darkGrey = Color(0xFF858585);
+  static const Color cyan = Color(0xFFB8B8B8);
 }
 
 class GradientColor {
